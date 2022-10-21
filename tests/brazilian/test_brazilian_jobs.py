2d6ca634514c4123b580c2628ed17ca2e97a9394
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-10-21 18:53:42 -0300

    feat: testes brazilian  e conter

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,13 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
     pass
+    brazilian_jobs_mock = read_brazilian_file(
+        "tests/mocks/brazilians_jobs.csv"
+    )
+    assert {
+        "title": "Auxiliar usinagem",
+        "salary": "1400",
+        "type": " full time",
+    } in brazilian_jobs_mock
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
     pass
+    print(count_ocurrences("src/jobs.csv", "java"))
+    print('sdijaiosdjaiojdoijsaiodjsaiodj')
+    assert count_ocurrences("src/jobs.csv", "java") != 10
+    assert count_ocurrences("src/jobs.csv", "javascript") == 122
