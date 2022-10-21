f58cb5ea6ad638b42723830b380b31826cb27421
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-10-21 14:58:52 -0300

    feat: Filtrando por tipo de trabalho

--- src/insights.py
+++ src/insights.py
@@ -8,21 +8,10 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    job_type_filtered = list(
+        job for job in jobs if job["job_type"] == job_type
+    )
+    return job_type_filtered
 
 
 def get_unique_industries(path):
